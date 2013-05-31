f7587bed2d3e21db8edd8f04fbf52ed737205670
Andy Reagan <github@andyreagan.com>
2013-05-31 12:51:57 -0400

    correctly loading in the LabMT dataset

---
+++ judge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# judge the happiness of tweets
+
+def readLabMT(stopval):
+  # read the labMT dataset into a dict, of the stopval
+  fname = 'labMT1_' + stopval + '.txt'
+  f = open(fname,'r')
+  tmp = dict([(line.split('\t')[0],[x.rstrip() for x in line.split('\t')[1:]]) for line in f])
+  return tmp
+
