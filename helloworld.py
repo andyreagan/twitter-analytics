ec939546081919cb4bcfa6df896994dbefdad9da
Andy Reagan <github@andyreagan.com>
2013-08-10 16:50:49 -0400

    dummy commit

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World! You suck!')
+        self.response.write('Hello, app engine!')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
